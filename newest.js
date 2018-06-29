8a7d40869de85e7ffafd1aa8cd8b6317b3bac6c6
Rodrigo Flores <rflor012@gmail.com>
2018-06-29 11:24:39 -0400

    combined

--- newest.js
+++ newest.js
@@ -1,15 +1,42 @@
-/* jshint esversion:6 */
-
 var lastClicked;
+
+//saw this 1
+var mapper = function mapMaker(){
+  // a = (Math.floor(Math.random()*3+1))
+  // switch(a) {
+  //   case 1: {var map = [
+  //                       [3,3,0,0,1],
+  //                       [0,1,1,0,2],
+  //                       [0,2,2,2,3],
+  //                       [1,4,0,1,0],
+  //                       [2,0,0,0,3],];}
+  //                       break;
+  //   case 2: {var map = [
+  //                       [3,0,3,3,1],
+  //                       [0,0,1,0,2],
+  //                       [1,2,2,2,1],
+  //                       [1,4,0,0,0],
+  //                       [2,0,3,0,3],];}
+  //                       break;
+  //   case 3: {var map = [
+  //                       [5,5,0,5,1],
+  //                       [0,1,1,0,2],
+  //                       [0,2,2,2,3],
+  //                       [1,4,5,1,0],
+  //                       [2,0,5,0,3],]
+  //                       break;} ;}
+  // return map;
+  // }
 
 
 var map = [
-  [3,3,0,0,3],
-  [0,1,1,0,3],
+  [3,3,0,0,1],
+  [0,1,1,0,2],
   [0,2,2,2,3],
   [1,4,0,1,0],
   [2,0,0,0,3],
 ];
+
 
 //just change to 5 incase something dies
 
@@ -62,8 +89,13 @@ function clickableGrid(rows, cols, map) {
 
 // all hail Nick!
 // map maker
-  document.getElementById("screen");
+function updateMap(map) {
+  clickableGrid(5, 5, map);
+  document.getElementById("screen")
   document.getElementById("screen").appendChild(grid);
+}
+
+
 
 // document.body.div[2].appendChild(grid);
 
@@ -83,7 +115,7 @@ function deadTileMaker(){
   x.setAttribute("src", "dead.png");
   x.setAttribute("width", "100px");
   x.setAttribute("height", "100px");
-  x.setAttribute("alt", "tree tile");
+  x.setAttribute("alt", "dead tile");
   return (x);
   }
 
@@ -100,7 +132,7 @@ return (x);
 function soldierTileMaker() {
 var x = document.createElement("IMG");
 x.setAttribute("src", "Hero.gif");
-x.setAttribute("class", "soldier");
+x.setAttribute("class", "hero");
 x.setAttribute("width", "45px");
 x.setAttribute("height", "45px");
 x.setAttribute("alt", "dirt tile");
@@ -121,17 +153,20 @@ return (x);
 function orcTileMaker() {
   var x = document.createElement("IMG");
   x.setAttribute("src", "orc.gif");
-  x.setAttribute("class", "enemy");
+  x.setAttribute("class", "orc");
   x.setAttribute("width", "50px");
   x.setAttribute("height", "50px");
   x.setAttribute("alt", "shrekt");
   // var stoneTiles = document.getElementsByClassName('porcy');
   return (x);
   }
+
+
+
 // GAME LOGIC Tricky part starts now ================================================================
 
 // var map = [
-//   [3,3,0,0,0],
+//   [3,3,0,1,1],
 //   [0,1,1,0,0],
 //   [0,2,2,2,3],
 //   [1,4,0,1,0],
@@ -185,7 +220,7 @@ function Soldier(name, health, damage, row, column) {
     if (this.health > 0){
       return this.name + " has received " + damage + " points of damage.";
     } else {
-      return this.name + "has succumbed to its wounds.";
+      return this.name + " has succumbed to its wounds.";
     }
   };
 
@@ -216,33 +251,14 @@ BattleField.prototype.attackEnemy = function () {
   for (var r = 0; r < map.length; r++) {
     for (var c = 0; c < map[r].length; c++) {
       if(enemyUnit.health <= 0){
-        $(".enemy").eq(enemyIndex).toggleClass('dead');
-        $(".enemy").eq(enemyIndex).prop("src", "dead.png");
         that.theDead.push(enemyUnit);
-        // that.enemy.splice(enemyIndex, 1);
-      } else if (armyUnit.health <= 0){
-        $(".soldier").eq(enemyIndex).toggleClass('dead');
-        $(".soldier").eq(enemyIndex).prop("src", "dead.png");
+        that.enemy.splice(enemyIndex, 1);
+      } else if (armyUnit.health <=0){
         that.theDead.push(armyUnit);
-        // that.army.splice(armyIndex, 1);
-
+        that.army.splice(armyIndex, 1);
       }
     }
   }
-    var attacking = $(".soldier").eq(armyIndex);
-    attacking.addClass("engager-indicator");
-    setTimeout(function(){
-      attacking.removeClass('engager-indicator');
-    }, 2500);
-
-    var defending = $(".enemy").eq(enemyIndex);
-    defending.addClass("combat-indicator");
-    defending.addClass("damage-sprite");
-    setTimeout(function(){
-      defending.removeClass("combat-indicator");
-      defending.removeClass("damage-sprite");
-    }, 2500);
-
     return battleResult;
 };
 
@@ -256,32 +272,15 @@ BattleField.prototype.enemyAttack = function() {
   for (var r = 0; r < map.length; r++) {
     for (var c = 0; c < map[r].length; c++) {
       if (armyUnit.health <= 0) {
-        $(".soldier").eq(enemyIndex).toggleClass('dead');
-        $(".soldier").eq(enemyIndex).prop("src", "dead.png");
         that.theDead.push(armyUnit);
-        // that.army.splice(armyIndex, 1);
+        that.army.splice(armyIndex, 1);
       } else if (enemyUnit.health <= 0) {
-        $(".enemy").eq(enemyIndex).toggleClass('dead');
-        $(".enemy").eq(enemyIndex).prop("src", "dead.png");
         that.theDead.push(enemyUnit);
-        // that.enemy.splice(enemyIndex, 1);
+        that.enemy.splice(enemyIndex, 1);
+
       }
     }
   }
-  var attacking = $(".enemy").eq(armyIndex);
-  attacking.addClass("engager-indicator");
-  setTimeout(function(){
-    attacking.removeClass('engager-indicator');
-  }, 1500);
-
-  var defending = $(".soldier").eq(enemyIndex);
-  defending.addClass("combat-indicator");
-  defending.addClass("damage-sprite");
-  setTimeout(function(){
-    defending.removeClass("combat-indicator");
-    defending.removeClass("damage-sprite");
-  }, 1500);
-};
 
   BattleField.prototype.combatStatus = function() {
     if (this.army.length === []) {
@@ -291,31 +290,44 @@ BattleField.prototype.enemyAttack = function() {
     } else {
       return false;
     }
+  };
   return battleResult;
 };
 
 
-// BattleField.prototype.combatStatus = function() {
-//   if (this.army.length === []){
-//     return "The Soldier's have died, the enemies win";
-//   } else if (this.enemy.length === []) {
-//     return "The enemies have all died, the Soldier's win";
-//   } else{
-//     return false;
-//   }
-// };
+BattleField.prototype.combatStatus = function() {
+  if (this.army.length === []){
+    return "The Soldier's have died, the enemies win";
+  } else if (this.enemy.length === []) {
+    return "The enemies have all died, the Soldier's win";
+  } else{
+    return false;
+  }
+};
 
 var theBattleField = new BattleField();
 
 var Soldier1 = new Soldier("Lucius IV", 100, 20, 1, 1);
 var Soldier2 = new Soldier("Anton II", 100, 20, 1, 2);
 var Soldier3 = new Soldier("Ramza III", 100, 20, 3, 0);
-var Soldier4 = new Soldier("Cloud I", 100, 20, 3, 3);
+var Soldier4 = new Soldier("Cloud I", 100, 20, 2, 3);
 
-var Enemy1 = new Enemy("Gritty", 100, 20, 2, 1);
+var Enemy1 = new Enemy("Grittyty", 100, 20, 2, 1);
 var Enemy2 = new Enemy("Watsanupdog", 100, 20, 2, 2);
 var Enemy3 = new Enemy("Inamaw", 100, 20, 2, 3);
 var Enemy4 = new Enemy("Panuway", 100, 20, 4, 0);
+var Enemy5 = new Enemy("OrcyPorcy", 100, 20, 4, 0);
+
+/*
+Soldier.name
+Soldier.damage
+Soldier.position
+
+Enemy.name
+Enemy.damage
+Enemy.position
+*/
+
 
 theBattleField.addSoldier(Soldier1);
 theBattleField.addSoldier(Soldier2);
@@ -327,52 +339,54 @@ theBattleField.addEnemy(Enemy2);
 theBattleField.addEnemy(Enemy3);
 theBattleField.addEnemy(Enemy4);
 
-
+//DOM statements with clicks
 function updateDOM() {
     for(var i = 0; i < theBattleField.army.length; i++){
-      $('.sol-box .name').eq(i).html(`<span class ='soldier-text-status'></span> `+theBattleField.army[i].name);
-      $('.sol-box .health').eq(i).html(`<span class ='soldier-text-status'></span> `+"HP: " +theBattleField.army[i].health);
-      $('.sol-box .strength').eq(i).html(`<span class ='soldier-text-status'></span> `+"DMG: "+theBattleField.army[i].damage);
+      $('.soldier-stat').eq(i).html("<span class ='soldier-text-status'></span> "+theBattleField.army[i].health);
+      $('.soldier-stat').eq(i).html("<span class ='soldier-text-status'></span> "+theBattleField.army[i].strength);
     }
-    for(var k = 0; k < theBattleField.enemy.length; k++){
-      $('.orc-stat .name').eq(k).html(`<span class ='soldier-text-status'></span> `+theBattleField.enemy[k].name);
-      $('.orc-stat .health').eq(k).html(`<span class ='soldier-text-status'></span> `+"HP: " +theBattleField.enemy[k].health);
-      $('.orc-stat .strength').eq(k).html(`<span class ='soldier-text-status'></span> `+"DMG: " +theBattleField.enemy[k].damage);
+    for(var i = 0; i < theBattleField.enemy.length; i++){
+      $('.orc-stat .health').eq(i).html("<span class ='soldier-text-status'></span> "+theBattleField.enemy[i].health);
+      $('.orc-stat .strength').eq(i).html("<span class ='soldier-text-status'></span> "+theBattleField.enemy[i].strength);
     }
-    // for(var i = 0; i < theBattleField.deadBodies; i++){
-    //   $('.death-box .health').eq(i).text('RIP');
-    //   $('.death-box .health').eq(i).text('RIP');
-    //   $('.death-box .strength').eq(i).text('RIP');
-    // }
+    for(var i = 0; i < theBattleField.deadBodies; i++){
+      $('.death-box .health').eq(i).html('RIP');
+      $('.death-box .strength').eq(i).html('RIP');
+    }
     if(theBattleField.combatStatus()){
       $('.text').text(theBattleField.combatStatus());
     }
 }
 
-$(document).ready(function(e){
-  updateDOM();
-  $('.qbtn').on('keydown', function(e){ $('.text').text(theBattleField.attackEnemy()); setTimeout(function(){ updateDOM();}, 1000);});
-  $('.pbtn').on('keydown', function(e){ $('.text').text(theBattleField.enemyAttack()); setTimeout(function(){ updateDOM();}, 1000);});
-},1500);
-
-
 $(document).ready(function(){
   updateDOM();
-  window.addEventListener('keydown',function(e){
-    var keys = document.querySelector(`button[data-key="${e.keyCode}"]`);
-    if(!keys) return;
+  $('.qbtn').on('click', function(){ $('.text').text(theBattleField.attackEnemy()); setTimeout(function(){ updateDOM();}, 2000);});
+  $('.pbtn').on('click', function(){ $('.text').text(theBattleField.enemyAttack()); setTimeout(function(){ updateDOM();}, 2000);
+  });
+  },1000);
+
+// DOM statements
+// $(document).ready(function(){
+//   updateDOM();
+//   window.addEventListener('keydown',function(e){
+//     var keys = document.querySelector(`button[data-key="${e.keyCode}"]`);
+//     if(!keys) return;
 //     // console.log(e);
-    $('.qbtn').on('keydown', function(e){ $(`.solder-stat`).text(theBattleField.attackEnemy());
-    setTimeout(function(){ updateDOM();}, 1000);});
-    $('.pbtn').on('keydown', function(a){ $(`.orc-stat`).text(theBattleField.attackEnemy());
-    setTimeout(function(){ updateDOM();}, 1000);});
-    });
+//     $('.qbtn').on('key-down', function(){ $(`.solder-stat`).text(theBattleField.attackEnemy());
+//     setTimeout(function(){ updateDOM();}, 2000);});
+//     $('.pbtn').on('key-down, function(){ $(`.orc-stat`).text(theBattleField.attackEnemy());
+//     setTimeout(function(){ updateDOM();}, 2000);});
+//     });
+//   });
+// }
+
+// window.addEventListener('keydown',function(e){
+//   var keys = document.querySelector(`button[data-key="${e.keyCode}"]`);
+//   if (keys === "81") {console.log("hello");} //{theBattleField.attackEnemy();}
+//     else if (keys === "80") {console.log(e);} //{theBattleField.enemyAttack();}
+
+
+// });
 
 //
-// window.addEventListener('keydown',function(e){
-//   var keys = document.querySelector("#qb");
-//   if (keys.which === "81") {console.log("hello");} //{theBattleField.attackEnemy();}
-//     else if (keys.which === "80") {console.log(e);} //{theBattleField.enemyAttack();}
-//
-});
-//
+};
